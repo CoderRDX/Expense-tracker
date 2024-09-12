a84949a7830e4e1c63d5b416ac9d3e596e114d3f
CoderRDX <rahuldev6258@gmail.com>
2024-09-12 23:38:32 +0530

    test commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import styles from './Home.module.css'
+import PieChart from "./PieChart";
+import BarChart from "./BarChart";
+import Card from './Card'
+import TransactionList from "./TransactionList";
+import ExpenseForm from "./ExpenseForm";
+import Modal from "./Modal";
+import AddBalanceForm from "./AddBalanceForm";
+
+
+export default function Home(){
+
+    
+    const useLocalStorage = (key, initialValue) => {
+        const [storedValue, setStoredValue] = useState(() => {
+          const item = localStorage.getItem(key);
+          return item ? JSON.parse(item) : initialValue;
+        });
+      
+        useEffect(() => {
+          localStorage.setItem(key, JSON.stringify(storedValue));
+        }, [key, storedValue]);
+      
+        return [storedValue, setStoredValue];
+      };
+
+    const [balance, setBalance] = useLocalStorage("balance", 5000);
+    const [expenseList, setExpenseList] = useLocalStorage("expenses", []);
+  
+
+    const [isOpenExpense, setIsOpenExpense] = useState(false);
+    const [isOpenBalance, setIsOpenBalance] = useState(false);
+
+      
+      const calculateCategoryStats = (expenseList) => {
+        return expenseList.reduce(
+          (acc, item) => {
+            acc.spends[item.category] =
+              (acc.spends[item.category] || 0) + Number(item.price);
+            acc.counts[item.category] = (acc.counts[item.category] || 0) + 1;
+            return acc;
+          },
+          {
+            spends: { food: 0, entertainment: 0, travel: 0 },
+            counts: { food: 0, entertainment: 0, travel: 0 },
+          }
+        );
+      };
+
+    console.time();
+    const { spends: categorySpends, counts: categoryCounts } =
+      calculateCategoryStats(expenseList);
+    console.timeEnd();
+    console.log({ categorySpends, categoryCounts });
+  
+    const expense = expenseList.reduce(
+      (total, item) => total + Number(item.price),
+      0
+    );
+  
+    const handleAddIncome = () => {
+      setIsOpenBalance(true);
+    };
+    const handleAddExpense = () => {
+      setIsOpenExpense(true);
+    };
+
+
+    return(
+        <div className={styles.container}>
+      <h1>Expense Tracker</h1>
+
+   
+      <div className={styles.cardsWrapper}>
+        <Card
+          title="Wallet Balance"
+          money={balance}
+          buttonText="+ Add Income"
+          buttonType="success"
+          handleClick={handleAddIncome}
+        />
+
+        <Card
+          title="Expenses"
+          money={expense}
+          buttonText="+ Add Expense"
+          buttonType="failure"
+          success={false}
+          handleClick={handleAddExpense}
+        />
+
+        <PieChart
+          data={[
+            { name: "Food", value: categorySpends.food },
+            { name: "Entertainment", value: categorySpends.entertainment },
+            { name: "Travel", value: categorySpends.travel },
+          ].filter((item) => item.value)}
+        />
+      </div>
+
+      <div className={styles.transactionsWrapper}>
+        <TransactionList
+          transactions={expenseList}
+          editTransactions={setExpenseList}
+          title="Recent Transactions"
+          balance={balance}
+          setBalance={setBalance}
+        />
+
+        <BarChart
+           data={[
+            { name: "Food", value: categorySpends.food },
+            { name: "Entertainment", value: categorySpends.entertainment },
+            { name: "Travel", value: categorySpends.travel },
+          ]}
+        />
+      </div>
+
+    
+      <Modal isOpen={isOpenExpense} setIsOpen={setIsOpenExpense}>
+        <ExpenseForm
+          setIsOpen={setIsOpenExpense}
+          expenseList={expenseList}
+          setExpenseList={setExpenseList}
+          setBalance={setBalance}
+          balance={balance}
+        />
+      </Modal>
+
+      <Modal isOpen={isOpenBalance} setIsOpen={setIsOpenBalance}>
+        <AddBalanceForm setIsOpen={setIsOpenBalance} setBalance={setBalance} />
+      </Modal>
+    </div>
+    ); 
+}
